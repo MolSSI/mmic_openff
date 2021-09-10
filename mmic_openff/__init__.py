b0bde8c6504cc67479b38087f1a13e4e1052fce8
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-09-10 15:59:54 -0400

    move versioning and ext map gens to mmic_openff.py

--- mmic_openff/__init__.py
+++ mmic_openff/__init__.py
@@ -7,27 +7,7 @@ Tactic MMIC for OpenFF/MMSchema translation.
 from . import components, models
 
 # Handle versioneer
-from ._version import get_versions
-from .mmic_openff import units
-from openff.toolkit.utils.toolkits import GLOBAL_TOOLKIT_REGISTRY
-
-versions = get_versions()
-__version__ = versions["version"]
-__git_revision__ = versions["full-revisionid"]
-del get_versions, versions
-
-
-def _gen_ext_maps(obj, toolkit_registry=GLOBAL_TOOLKIT_REGISTRY):
-    toolkit = None
-    ext_map = {}
-
-    for query_toolkit in toolkit_registry.registered_toolkits:
-        ext_map.update(
-            {"." + val.lower(): val.lower() for val in getattr(query_toolkit, obj)}
-        )
-
-    return ext_map
-
+from .mmic_openff import __version__, units, _gen_ext_maps
 
 # Need to read these from toolkit.utils.ToolkitWrapper.toolkit_file_read_formats, etc.
 molread_ext_maps = _gen_ext_maps("toolkit_file_read_formats")
--- mmic_openff/mmic_openff.py
+++ mmic_openff/mmic_openff.py
@@ -5,6 +5,14 @@ A short description of the project.
 Handles the primary functions
 """
 
+from openff.toolkit.utils.toolkits import GLOBAL_TOOLKIT_REGISTRY
+from ._version import get_versions
+
+versions = get_versions()
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
+del get_versions, versions
+
 # Incorrect ~ must update
 units = {
     "length": "angstrom",
@@ -15,3 +23,15 @@ units = {
     "angle": "radians",
     "temperature": "kelvin",
 }
+
+
+def _gen_ext_maps(obj, toolkit_registry=GLOBAL_TOOLKIT_REGISTRY):
+    toolkit = None
+    ext_map = {}
+
+    for query_toolkit in toolkit_registry.registered_toolkits:
+        ext_map.update(
+            {"." + val.lower(): val.lower() for val in getattr(query_toolkit, obj)}
+        )
+
+    return ext_map
