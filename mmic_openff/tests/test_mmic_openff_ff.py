1c8dece67bdf2e550afcf7ca0ce425e89cc0f9b1
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-09-13 15:31:46 -0400

    test for OpenFFToFFComponent

--- mmic_openff/tests/test_mmic_openff_ff.py
+++ mmic_openff/tests/test_mmic_openff_ff.py
@@ -4,31 +4,35 @@ Unit and regression test for the mmic_openff package.
 
 # Import package, test suite, and other packages as needed
 import mmic_openff
+from openff.toolkit.typing.engines.smirnoff import ForceField
 import pytest
 import sys
 import os
 import mmelemental as mm
 import mm_data
 
-amber99sb = mm_data.ffs["amber99sb.xml"]
+forcefields = [
+    "openff-2.0.0.offxml",
+    #    pytest.param(mm_data.ffs["amber99sb.xml"], marks=pytest.mark.skip),
+]
 
 
-@pytest.mark.skip
-def test_mmic_to_ff_from_xml(**kwargs):
+@pytest.mark.parametrize("ff", forcefields)
+def test_mmic_to_ff_from_xml(ff, **kwargs):
     inputs = {
-        "data_object": ForceField(amber99sb),
+        "data_object": ForceField(ff),
         "keywords": kwargs,
         "schema_version": 1,
-        "schema_name": "my_schema",
+        "schema_name": "mmschema",
     }
-    return mmic_openff.components.OpenMMToFFComponent.compute(inputs)
+    return mmic_openff.components.OpenFFToFFComponent.compute(inputs)
 
 
 @pytest.mark.skip("Cannot test this right now.")
 def test_ff_to_openmm(**kwargs):
     ff = mm.models.ForceField.from_file(amber99sb)
     inputs = {"schema_object": ff, "keywords": kwargs}
-    return mmic_openff.components.MolToOpenMMComponent.compute(inputs)
+    return mmic_openff.components.FFToOpenFFComponent.compute(inputs)
 
 
 @pytest.mark.skip
