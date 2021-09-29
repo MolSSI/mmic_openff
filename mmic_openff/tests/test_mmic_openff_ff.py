2df4f8337b988893639fca40f817074e2eaf9c3d
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-09-29 17:05:34 -0400

    enable testing for FFToOpenFFComponent

--- mmic_openff/tests/test_mmic_openff_ff.py
+++ mmic_openff/tests/test_mmic_openff_ff.py
@@ -19,24 +19,30 @@ forcefields = [
 
 
 @pytest.mark.parametrize("ff", forcefields)
-def test_mmic_to_ff_from_xml(ff, **kwargs):
+def test_mmic_openff_from_xml(ff, **kwargs):
     inputs = {
         "data_object": ForceField(ff),
         "keywords": kwargs,
-        "schema_version": 1,
+        "schema_version": mmic_openff.mmic_openff._mmschema_max_version,
         "schema_name": mm.models.ForceField.default_schema_name,
     }
     return mmic_openff.components.OpenFFToFFComponent.compute(inputs)
 
 
-@pytest.mark.skip("Cannot test this right now.")
-def test_ff_to_openmm(**kwargs):
-    ff = mm.models.ForceField.from_file(amber99sb)
-    inputs = {"schema_object": ff, "keywords": kwargs}
+@pytest.mark.parametrize("ff", forcefields)
+def test_mmic_openff_from_mmschema(ff, **kwargs):
+    mmff = test_mmic_openff_from_xml(ff).schema_object
+    mmff = mm.models.ForceField(**mmff.dict())
+    inputs = {
+        "schema_object": mmff,
+        "schema_name": mmff.schema_name,
+        "schema_version": mmff.schema_version,
+        "keywords": kwargs,
+    }
     return mmic_openff.components.FFToOpenFFComponent.compute(inputs)
 
 
 @pytest.mark.skip
 def test_io_methods(**kwargs):
     ff = mmic_openff.models.OpenMMFF.from_file(amber99sb)
-    assert isinstance(ff.data, ff.dtype())
+    assert isinstance(ff.data, ff.dtype)
